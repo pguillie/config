255363b9f7fada1e836f48b87809f1b7e4b9b7ae
Paul Guillier <paul.guillier@protonmail.com>
2019-09-19 13:44:41 -0400

    install script is working for tmux and emacs

--- install.sh
+++ install.sh
@@ -2,60 +2,76 @@
 
 function set_emacs
 {
-	if [[ $EUID -eq 0 ]]; then
-		echo "Enter the HOME directory of the user" \
-		     "you want to have EMACS configurated"
-		read home
-	else
-		home=$HOME
-	fi
+    if [[ $EUID -eq 0 ]]; then
+	echo "Enter the HOME directory of the user" \
+	     "you want to have EMACS configurated"
+	read home
+    else
+	home=$HOME
+    fi
 
-	read -p 'Do you need the 42 configuration? (Y/n)' 42
+    read -p 'Do you need the 42 header? (Y/n)' header
 
-	if [ -e $home/.emacs ]; then
-		echo "WARNING: Another configuration file exists ($home/.emacs)"
-		read -p 'Override? (Y/n)' override
-		if ! [[ $override =~ ^[Yy] ]]; then
-			echo "[-] Aborting EMACS configuration" 1>&2
-			return
-		fi
+    echo "[+] Create $home/.emacs file"
+    if [ -e $home/.emacs ]; then
+	echo "[!] Another configuration file exists ($home/.emacs)"
+	read -p 'Override? (Y/n)' override
+	if ! [[ $override =~ ^[Yy] ]]; then
+	    echo "[-] Aborting EMACS configuration" 1>&2
+	    return
 	fi
-	#...
+    fi
+    cp emacs/dotemacs $home/.emacs
+
+    echo "[+] Create $home/.emacs.d/init directory"
+    if ! [ -d $home/.emacs.d ]; then
+	mkdir $home/.emacs.d
+    fi
+    cp -r emacs/emacs.d/init $home/.emacs.d
+
+    echo "[+] Create $home/.emacs.d/42-header directory"
+    if [[ $header =~ ^[Yy] ]]; then
+	cp -r emacs/emacs.d/42-header $home/.emacs.d
+    fi
+
+    echo "[*] EMACS successfully configurated!"
 }
 
 function set_tmux
 {
-	if [[ $EUID -eq 0 ]]; then
-		conf_file="/etc/tmux.conf"
-	else
-		conf_file="$HOME/.tmux.conf"
-	fi
+    if [[ $EUID -eq 0 ]]; then
+	conf_file="/etc/tmux.conf"
+    else
+	conf_file="$HOME/.tmux.conf"
+    fi
 
-	if [ -e $conf_file ]; then
-		echo "WARNING: Another configuration file exists ('$conf_file')"
-		read -p 'Override? (Y/n)' override
-		if ! [[ $override =~ ^[Yy] ]]; then
-			echo "[-] Aborting TMUX configuration" 1>&2
-			return
-		fi
+    echo "[+] Create $conf_file file"
+    if [ -e $conf_file ]; then
+	echo "[!] Another configuration file exists ('$conf_file')"
+	read -p 'Override? (Y/n)' override
+	if ! [[ $override =~ ^[Yy] ]]; then
+	    echo "[-] Aborting TMUX configuration" 1>&2
+	    return
 	fi
-	cp tmux/tmux.conf $conf_file
-	echo "[+] TMUX successfully configurated in $conf_file"
+    fi
+    cp tmux/tmux.conf $conf_file
+
+    echo "[*] TMUX successfully configurated!"
 }
 
 if [[ -n $@ ]]; then
-	argv="$@"
+    argv="$@"
 else
-	argv="emacs tmux"
+    argv="emacs tmux"
 fi
 
 for arg in $argv; do
-	echo "[== ${arg^^} ==]"
-	if [ $arg == "emacs" ]; then
-		set_emacs
-	elif [ $arg == 'tmux' ]; then
-		set_tmux
-	else
-		echo "ERROR: No configuration provided for \"$arg\"" 1>&2
-	fi
+    echo "[== ${arg^^} ==]"
+    if [ $arg == "emacs" ]; then
+	set_emacs
+    elif [ $arg == 'tmux' ]; then
+	set_tmux
+    else
+	echo "ERROR: No configuration provided for \"$arg\"" 1>&2
+    fi
 done
