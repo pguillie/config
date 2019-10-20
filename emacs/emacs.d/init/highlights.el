5369d6975b981b89390af764a2389fc5dc00a533
Paul Guillier <paul.guillier@protonmail.com>
2019-10-20 10:33:31 -0400

    feat: highlight current line and bracket blocks

---
+++ emacs/emacs.d/init/highlights.el
@@ -0,0 +1,10 @@
+;; Highlights
+
+;; current line
+(global-hl-line-mode t)
+(set-face-background 'hl-line "#424242")
+
+;; matching brackets
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
